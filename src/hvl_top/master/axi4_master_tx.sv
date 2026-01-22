c1db984308800a086e9dd58505961e091ad53728
Swasthikkamath <118376716+Swasthikkamath@users.noreply.github.com>
2026-01-23 04:06:12 +0530

    update

--- src/hvl_top/master/axi4_master_tx.sv
+++ src/hvl_top/master/axi4_master_tx.sv
@@ -218,7 +218,7 @@ class axi4_master_tx extends uvm_sequence_item;
   //-------------------------------------------------------
   //Constraint : awaddr
   //Used to generate the alligned address with respect to size
-  constraint awaddr_c0 {soft awaddr inside {104};}
+  constraint awaddr_c0 {soft awaddr inside {104,107,110,150,210};}
 
   //Constraint : awburst_c1
   //Restricting write burst to select only FIXED, INCR and WRAP types
@@ -277,7 +277,7 @@ class axi4_master_tx extends uvm_sequence_item;
   
   //Constraint : araddr
   //Used to generate the alligned address with respect to size
-  constraint araddr_c0 {soft araddr  == awaddr;}
+  constraint araddr_c0 {soft araddr inside {104,103,110,200,250,140};}
   
   //Constraint : arburst_c1
   //Restricting read burst to select only FIXED, INCR and WRAP types
